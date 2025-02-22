280acabd0a34f9e83e57076e78afad9de6ab952f
gs256 <51964952+gs256@users.noreply.github.com>
2025-02-22 21:18:15 +0300

    Shorten functionality on client

--- client/src/app/home-page/home-page.component.ts
+++ client/src/app/home-page/home-page.component.ts
@@ -1,18 +1,50 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { ApiService } from '../services/api/api.service';
+import { finalize } from 'rxjs';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-home-page',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.scss',
 })
 export class HomePageComponent {
   private readonly api = inject(ApiService);
 
+  public readonly processing = signal(false);
+
+  public readonly shortLink = signal('');
+
+  public readonly originalUrl = signal('');
+
   public onShortenClicked() {
-    this.api.shorten('https://example.com').subscribe((result) => {
-      console.log('result:', result);
-    });
+    if (this.processing()) {
+      return;
+    }
+    if (this.originalUrl().trim().length == 0) {
+      alert('Enter your url');
+      return;
+    }
+    this.processing.set(true);
+    this.api
+      .shorten(this.originalUrl())
+      .pipe(
+        finalize(() => {
+          this.processing.set(false);
+        })
+      )
+      .subscribe({
+        next: (result) => {
+          this.shortLink.set(this.getShortUrl(result));
+        },
+        error: (error: Error) => {
+          alert(error.message);
+        },
+      });
+  }
+
+  private getShortUrl(alias: string) {
+    return `${window.location.origin}/${alias}`;
   }
 }
--- client/src/app/services/api/api.service.ts
+++ client/src/app/services/api/api.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { catchError, map, Observable } from 'rxjs';
 import { API_URL } from '../../constants';
@@ -9,10 +9,12 @@ import { API_URL } from '../../constants';
 export class ApiService {
   private readonly http = inject(HttpClient);
 
-  shorten(url: string): Observable<string> {
+  public shorten(url: string): Observable<string> {
     return this.http.post(`${API_URL}/api/v1/shorten`, { url: url }).pipe(
-      catchError((error) => {
-        throw error;
+      catchError((error: HttpErrorResponse) => {
+        const message = this.getErrorResponseMessage(error);
+        if (message) throw new Error(message);
+        throw new Error('Unknown error occured');
       }),
       map((res) => {
         if ('alias' in res && typeof res['alias'] == 'string' && res['alias']) {
@@ -23,7 +25,7 @@ export class ApiService {
     );
   }
 
-  test(): Observable<string> {
+  public test(): Observable<string> {
     return this.http
       .get(`${API_URL}/api/v1/test`, { responseType: 'text' })
       .pipe(
@@ -31,5 +33,18 @@ export class ApiService {
           throw error;
         })
       );
+  }
+
+  private getErrorResponseMessage(
+    error: HttpErrorResponse
+  ): string | undefined {
+    if (error.status == 0) {
+      return undefined;
+    }
+    const errorBody = error.error;
+    if ('error' in errorBody && typeof errorBody['error'] == 'string') {
+      return errorBody['error'] as string;
+    }
+    return undefined;
   }
 }
