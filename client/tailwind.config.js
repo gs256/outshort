0102d162fd9621bd28cb25caf828ae3018866610
gs256 <51964952+gs256@users.noreply.github.com>
2025-02-27 23:07:30 +0300

    Integration with tailwind 3

---
+++ client/tailwind.config.js
@@ -0,0 +1,10 @@
+import PrimeUI from "tailwindcss-primeui";
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,ts}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [PrimeUI],
+};
