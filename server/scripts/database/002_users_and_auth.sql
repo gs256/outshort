1a8faf667ca5ddc5e752d76285d984fa00454a27
gs256 <51964952+gs256@users.noreply.github.com>
2025-03-01 11:57:28 +0300

    Add users to db

---
+++ server/scripts/database/002_users_and_auth.sql
@@ -0,0 +1,33 @@
+CREATE TABLE users (
+    id INTEGER PRIMARY KEY AUTOINCREMENT UNIQUE,
+    username TEXT UNIQUE NOT NULL
+);
+
+CREATE TABLE auth_tokens (
+    id INTEGER PRIMARY KEY AUTOINCREMENT UNIQUE,
+    token TEXT UNIQUE NOT NULL,
+    user_id INTEGER NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    lifetime_sec INTEGER NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
+);
+
+
+-- Alter links table
+
+CREATE TABLE links_new (
+    id INTEGER PRIMARY KEY AUTOINCREMENT UNIQUE,
+    alias TEXT UNIQUE NOT NULL,
+    original_url TEXT NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    owner_id INTEGER,
+    FOREIGN KEY (owner_id) REFERENCES users(id) ON DELETE CASCADE
+);
+
+INSERT INTO links_new (id, alias, original_url, created_at)
+SELECT id, alias, original_url, created_at FROM links;
+
+DROP TABLE links;
+
+ALTER TABLE links_new RENAME TO links;
+
