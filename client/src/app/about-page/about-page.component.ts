fdfba62b6663835d65c6cc4a662a444720b53733
gs256 <51964952+gs256@users.noreply.github.com>
2025-03-02 16:09:30 +0300

    About page

--- client/src/app/about-page/about-page.component.ts
+++ client/src/app/about-page/about-page.component.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
 import { PageWrapperComponent } from '../page-wrapper/page-wrapper.component';
 import { MenubarComponent } from '../menubar/menubar.component';
+import { CardModule } from 'primeng/card';
+import { VERSION } from '../constants';
 
 @Component({
   selector: 'app-about-page',
-  imports: [PageWrapperComponent, MenubarComponent],
+  imports: [PageWrapperComponent, MenubarComponent, CardModule],
   templateUrl: './about-page.component.html',
   styleUrl: './about-page.component.scss',
 })
-export class AboutPageComponent {}
+export class AboutPageComponent {
+  public readonly version = VERSION;
+}
--- client/src/app/constants.ts
+++ client/src/app/constants.ts
@@ -1,3 +1,7 @@
+import { version } from '../../package.json';
+
+export const VERSION = version;
+
 export const API_URL = 'http://127.0.0.1:8249';
 
 export const ROUTES = {
